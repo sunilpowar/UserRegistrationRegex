1dd137db72dff0fb2f1079764e00080acefd44d9
sunilpowar <sunilpowarsp8468@gmail.com>
2022-02-11 23:51:33 +0530

    Adding UC8

--- UserRegistrationRegex/ValidationRegex.cs
+++ UserRegistrationRegex/ValidationRegex.cs
@@ -44,10 +44,11 @@ namespace UserRegistrationRegex
         }
         public void Checkpassward(string Passward)
         {
-            //string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
-            // string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}"; 
-            //string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-            string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            // string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
+            // string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            //  string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}"; 
+            // string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
